f7508eeb2fdfc6b4fdd6f1c7ee7d06d68d833adb
狐日泽 <Sun_Set1102@outlook.com>
2021-05-17 00:49:28 +0800

    Update ProTip.tsx

--- src/components/ProTip.tsx
+++ src/components/ProTip.tsx
@@ -10,33 +10,4 @@ const useStyles = makeStyles((theme) => ({
     justifyContent: "center",
     alignItems: "center",
   },
-}));
-
-/**
- * 直播间链接
- */
-function CaihuaLink() {
-  return (
-    <a
-      className="ximalaya-text-link"
-      href={caihuaLink}
-      target="_blank"
-      onClick={() => {
-        jumpToCaihua();
-      }}
-    >
-      菜花
-    </a>
-  );
-}
-
-export default function ProTip() {
-  const classes = useStyles();
-  return (
-    <Typography align="center" className={classes.root} color="textSecondary">
-      <EmojiObjectsOutlinedIcon />
-      Tip: {process.env.REACT_APP_DISABLE_ADSENSE ? null : <CaihuaLink />}
-      为你的夏日带去清凉！
-    </Typography>
-  );
-}
+}))
